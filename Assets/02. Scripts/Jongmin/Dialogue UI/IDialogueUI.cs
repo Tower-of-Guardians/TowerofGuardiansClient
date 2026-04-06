36d9c38043e791c7a02c9a26af4859966f2c2ef7
Jongmin Kim <vwmartin@naver.com>
2026-04-06 22:57:33 +0900

    remove: Yarn Spinner 패키지 제거 및 의존 코드 제거

--- Assets/02. Scripts/Jongmin/Dialogue UI/IDialogueUI.cs
+++
@@ -1,6 +0,0 @@
-public interface IDialogueUI
-{
-    void InitPortrait(CharacterCode player_code, CharacterCode npc_code);
-    void StartDialogue(string dialogue_code);
-    void StopDialogue();
-}
