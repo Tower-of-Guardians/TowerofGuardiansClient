2fdeb7861734879103eac34925ac68d91ea1b7fb
Jongmin Kim <vwmartin@naver.com>
2026-06-06 11:42:10 +0900

    feat: 공용 카드 추가

---
+++ Assets/02. Scripts/Jongmin/UI/Card/Card.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Jongmin
+{
+    public class Card : MonoBehaviour
+    {
+        [FormerlySerializedAs("model")] [SerializeField] private CardView view;
+        [SerializeField] private CardPointer pointer;
+
+        public BattleCardData BattleCardData { get; private set; }
+        public CardData CardData { get; private set; }
+        
+        public CardPointer Pointer => pointer;
+        
+        private void Awake()
+        {
+            view ??= GetComponent<CardView>();
+            pointer ??= GetComponent<CardPointer>();
+        }
+
+        public void SetBattleCardData(BattleCardData battleCardData)
+        {
+            BattleCardData = battleCardData;
+            CardData = battleCardData?.data;
+            view.UpdateModel(CardData);
+        }
+
+        public void SetCardData(CardData cardData)
+        {
+            BattleCardData = null;
+            CardData = cardData;
+            view.UpdateModel(CardData);
+        }
+
+        private void OnDisable()
+        {
+            BattleCardData = null;
+            CardData = null;
+        }
+    }
+}
---
+++ Assets/02. Scripts/Jongmin/UI/Card/CardPointer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using JxModule;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Jongmin
+{
+    [RequireComponent(typeof(JxEmptyGraphic))]
+    public class CardPointer : MonoBehaviour, 
+                               IPointerEnterHandler, 
+                               IPointerExitHandler, 
+                               IPointerClickHandler, 
+                               IPointerDownHandler, 
+                               IPointerUpHandler, 
+                               IBeginDragHandler, 
+                               IDragHandler, 
+                               IEndDragHandler
+    {
+        public event Action<PointerEventData> OnPointerEntered;
+        public event Action<PointerEventData> OnPointerExited;
+        public event Action<PointerEventData> OnPointerClicked;
+        public event Action<PointerEventData> OnPointerDowned;
+        public event Action<PointerEventData> OnPointerUpped;
+        public event Action<PointerEventData> OnBeginDragged;
+        public event Action<PointerEventData> OnDragged;
+        public event Action<PointerEventData> OnEndDragged;
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            OnPointerEntered?.Invoke(eventData);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            OnPointerExited?.Invoke(eventData);
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnPointerClicked?.Invoke(eventData);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            OnPointerDowned?.Invoke(eventData);
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            OnPointerUpped?.Invoke(eventData);
+        }
+
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            OnBeginDragged?.Invoke(eventData);
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            OnDragged?.Invoke(eventData);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            OnEndDragged?.Invoke(eventData);
+        }
+    }
+}
